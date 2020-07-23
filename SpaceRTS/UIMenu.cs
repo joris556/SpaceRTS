00707c21a2f9379ffca8187bd1538cd2d49732ed
Fjodor van Rijsselberg <fjodor_rs@hotmail.com>
2020-07-23 12:39:43 +0200

    ui

---
+++ SpaceRTS/UIMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace SpaceRTS
+{
+	class UIMenu
+	{
+		protected List<UIObject> UIObjects;
+		protected Vector2 offset;
+		protected Vector2 cameraOffset;
+		protected float scale;
+		protected bool mainScreen;
+
+		public Vector2 Offset { get => offset; set => offset = value; }
+		public float Scale { get => scale; set => scale = value; }
+		public Vector2 CameraOffset { get => cameraOffset; set => cameraOffset = value; }
+		public bool MainScreen { get => mainScreen; set => mainScreen = value; }
+		public List<UIObject> UIOBJECTS{ get => UIObjects; set => UIObjects = value; }
+
+		public void Update(GameTime gt)
+		{
+			foreach (UIObject g in UIObjects)
+				g.Update(gt);
+		}
+
+		public void HandleInput(InputHelper ih)
+		{
+			foreach (UIObject g in UIObjects)
+				g.HandleInput(ih);
+		}
+
+		public void Draw(GameTime gt, SpriteBatch sb)
+		{
+
+			foreach (UIObject g in UIObjects)
+				g.Draw(gt, sb);
+
+		}
+
+	}
+}
---
+++ SpaceRTS/UIObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceRTS
+{
+	class UIObject
+	{
+		protected Vector2 position;
+		protected Texture2D sprite;
+		protected Rectangle clickbox, spritebox;
+		protected float rotation;
+		protected bool visible = true;
+		protected Vector2 origin;
+		protected bool mainScreen;
+
+
+
+		public Vector2 Position { get => position; set { position = value; clickbox.X = (int)position.X; clickbox.Y = (int)position.Y; } }
+		public Texture2D Sprite { get => sprite; set => sprite = value; }
+		public Rectangle Clickbox { get => clickbox; set => clickbox = value; }
+		public Rectangle Spritebox { get => spritebox; set => spritebox = value; }
+		public bool Visible { get => visible; set => visible = value; }
+		public Vector2 Origin { get => origin; set => origin = value; }
+		public float Rotation { get => rotation; set => rotation = value; }
+		public bool MainScreen { get => mainScreen; set => mainScreen = value; }
+
+		public UIObject()
+		{
+
+		}
+
+		public virtual void Update(GameTime gt)
+		{
+
+		}
+
+		public virtual void HandleInput(InputHelper ih)
+		{
+
+		}
+
+		public virtual void Draw(GameTime gt, SpriteBatch sb)
+		{
+			if (visible)
+			{
+				sb.Draw(sprite, position + origin, spritebox, Color.White, rotation, origin, Constants.scale, SpriteEffects.None, 0f);
+			}
+		}
+	}
+}
